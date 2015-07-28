72f78bd75033958280c41a1120c9c56d9c74aae4
Bogdan Dever <bogvsdev@gmail.com>
2015-07-28 23:17:08 +0300

    editing

--- lapp/app/config/database.php
+++ lapp/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'lapp',
-			'username'  => 'root',
-			'password'  => 'bdevit',
+			'database'  => 'YOUR DB NAME',
+			'username'  => 'YOUR DB USERNAME',
+			'password'  => 'YOUR DB PASSWORD',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
